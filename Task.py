6f432d31265041165b60792f5e4d292c6926f7da
ИнтернетВсе <vskovrov@yandex.ru>
2023-02-13 08:32:15 +0300

    3 commit

--- Task.py
+++ Task.py
@@ -1,28 +1,22 @@
-violator_songs = [
-    ['a1', 4.86], 
-    ['a2', 4.43],
-    ['a3', 4.56],
-    ['a4', 4.9],
-    ['a5', 6.07],
-    ['a6', 4.20],
-    ['a7', 4.76],
-    ['a8', 4.29],
-    ['a9', 5.83]
-]
-
 # ввод плейлиста
 def input_playlist():
-    number_of_songs = int(input('Сколько песен выбрать?'))
-    list = [input(f'Название {i+1}-й песни:') for i in range(number_of_songs)]
+    number_of_songs = int(input('Сколько песен выбрать? '))
+    list = [input(f'Название {i+1}-й песни: ') for i in range(number_of_songs)]
     return list
 
+# считаем время
 def time_playlist(pl):
-    for i in range(len(pl)):
-        print(pl[i])
-    # return pl
+    count_time = 0
+    violator_songs = [['s1', 4.86], ['s2', 4.43], ['s3', 4.56], ['s4', 4.9], ['s5', 6.07],
+    ['s6', 4.20], ['s7', 4.76], ['s8', 4.29], ['s9', 5.83]]
+
+    for song_number in range(len(pl)):
+        for song_time in range(len(violator_songs)):
+            if pl[song_number] in violator_songs[song_time][0]:
+                count_time += float(violator_songs[song_time][1])
+    return count_time
 
 play_list = input_playlist()
 time = time_playlist(play_list)
 
-# print(play_list)
-# print(time)
+print(f'Общее время звучания песен: {round(time, 2)} минуты')
